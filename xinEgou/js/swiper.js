023cc68bfa0ea08924d53c930cbe84ce36a425b0
WishQAQ <wish-time@outlook.com>
2018-07-10 16:10:31 +0800

    新E购-mobile

---
+++ xinEgou/js/swiper.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('#banner_swiper',{   //banner
+    autoplay: true,
+})
+
+var swiper = new Swiper('#main_2_swiper', {  //垂直切换广告
+    direction: 'vertical',
+    autoplay: true,
+});
